01077798bce933aada8e4dc50421d8a3424e66f0
JaeyoungJeong <jjy9678@naver.com>
2020-03-10 00:57:57 +0900

    ai file

---
+++ main/static/js/base.js
@@ -0,0 +1,7 @@
+// toggle 버튼 설정
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-linear_regression-step2.proof
+$(function () {
+    $("#button-linear-step2-proof").on("click", function () {
+        $("#div-linear-step2-proof").toggleClass("disp-none");
+    });
+});
