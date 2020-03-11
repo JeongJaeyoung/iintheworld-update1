23bfddff29a9223436809f74229a34486f9e9f6a
JaeyoungJeong <jjy9678@naver.com>
2020-03-11 10:31:04 +0900

    design update

--- main/static/js/base.js
+++ main/static/js/base.js
@@ -1,13 +1,108 @@
-// toggle 버튼 설정
-// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-linear_regression-step2.proof
+
+// SCROLL EVENT if문이 모바일, else가 데스크탑일 때
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        var didScroll;
+        var lastScrollTop = 0;
+        var delta = 5;
+        var navbarHeight = $('header').outerHeight();
+
+        $(window).scroll(function (event) {
+            didScroll = true;
+        });
+
+        setInterval(function () {
+            if (didScroll) {
+                hasScrolled();
+                didScroll = false;
+            }
+        }, 250);
+
+        function hasScrolled() {
+            var st = $(this).scrollTop();
+
+            // Make sure they scroll more than delta
+            if (Math.abs(lastScrollTop - st) <= delta)
+                return;
+
+            // If they scrolled down and are past the navbar, add class .nav-up.
+            // This is necessary so you never see what is "behind" the navbar.
+            if (st > lastScrollTop && st > navbarHeight) {
+                // Scroll Down
+                $('header').removeClass('nav--down').addClass('nav--up');
+            } else {
+                // Scroll Up
+                if (st + $(window).height() < $(document).height()) {
+                    $('header').removeClass('nav--up').addClass('nav--down');
+                }
+            }
+
+            lastScrollTop = st;
+        }
+    } else {
+        var didScroll;
+        var lastScrollTop = 0;
+        var delta = 5;
+        var navbarHeight = $('header').outerHeight();
+
+        $(window).scroll(function (event) {
+            didScroll = true;
+        });
+
+        setInterval(function () {
+            if (didScroll) {
+                hasScrolled();
+                didScroll = false;
+            }
+        }, 250);
+
+        function hasScrolled() {
+            var st = $(this).scrollTop();
+
+            // Make sure they scroll more than delta
+            if (Math.abs(lastScrollTop - st) <= delta)
+                return;
+
+            // If they scrolled down and are past the navbar, add class .nav-up.
+            // This is necessary so you never see what is "behind" the navbar.
+            if (st > lastScrollTop && st > navbarHeight) {
+                // Scroll Down
+                $('header').removeClass('nav-down').addClass('nav-up');
+            } else {
+                // Scroll Up
+                if (st + $(window).height() < $(document).height()) {
+                    $('header').removeClass('nav-up').addClass('nav-down');
+                }
+            }
+
+            lastScrollTop = st;
+        }
+    }
+}
+
+var x = window.matchMedia("(max-width: 980px)")
+myFunction(x)
+x.addListener(myFunction) 
+
+// GNB-FRIST_ROW - HAMBURGER_BUTTON - LARGE CSS
 $(function () {
-    $("#button-linear-step2-matrix").on("click", function () {
-        $("#div-linear-step2-matrix").toggleClass("disp-none");
+    $("#first_row-hamburger_button-large_css").on("click", function () {
+        $(".sub-menu").toggleClass("hide");
     });
 });
-// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-linear_regression-step3.proof
+
+// GNB-FIRST_ROW - HAMBURGER_BUTTON - SMALL CSS
 $(function () {
-    $("#button-linear-step3-proof").on("click", function () {
-        $("#div-linear-step3-proof").toggleClass("disp-none");
+    $(".sample-class .btn-menu").on("click", function () {
+        $(".sample-class").toggleClass("menuon");
+    });
+});
+
+
+
+// GNB-SECOND_ROW - TOPIC_BUTTON
+$(function () {
+    $(".topic-menu").on("click", function () {
+        $(".topic-class").toggleClass("menu_on");
     });
 });
---
+++ main/static/js/p3-ch2-s2-ai.js
@@ -0,0 +1,13 @@
+// toggle 버튼 설정
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-linear_regression-step2.proof
+$(function () {
+    $("#button-linear-step2-matrix").on("click", function () {
+        $("#div-linear-step2-matrix").toggleClass("disp-none");
+    });
+});
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-linear_regression-step3.proof
+$(function () {
+    $("#button-linear-step3-proof").on("click", function () {
+        $("#div-linear-step3-proof").toggleClass("disp-none");
+    });
+});
