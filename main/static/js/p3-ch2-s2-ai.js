fc58806e01cd9eddde6cb36bac1330c47303e7aa
JaeyoungJeong <jjy9678@naver.com>
2020-03-12 20:41:46 +0900

    upload

--- main/static/js/p3-ch2-s2-ai.js
+++ main/static/js/p3-ch2-s2-ai.js
@@ -5,15 +5,45 @@ $(function () {
         $("#div-linear-step2-matrix").toggleClass("disp-none");
     });
 });
+
 // part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-linear_regression-step3.proof
 $(function () {
     $("#button-linear-step3-proof").on("click", function () {
         $("#div-linear-step3-proof").toggleClass("disp-none");
     });
 });
+
 // part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-linear_regression-step3.range
 $(function () {
     $("#button-linear-step3-range").on("click", function () {
         $("#div-linear-step3-range").toggleClass("disp-none");
+    });
+});
+
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-logistic-step1-matrix
+$(function () {
+    $("#button-logistic-step1-matrix").on("click", function () {
+        $("#div-logistic-step1-matrix").toggleClass("disp-none");
+    });
+});
+
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-logistic-step2-matrix
+$(function () {
+    $("#button-logistic-step2-matrix").on("click", function () {
+        $("#div-logistic-step2-matrix").toggleClass("disp-none");
+    });
+});
+
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-logistic-step3-proof
+$(function () {
+    $("#button-logistic-step3-proof").on("click", function () {
+        $("#div-logistic-step3-proof").toggleClass("disp-none");
+    });
+});
+
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-neural-step3-range
+$(function () {
+    $("#button-neural-step3-range").on("click", function () {
+        $("#div-neural-step3-range").toggleClass("disp-none");
     });
 });
